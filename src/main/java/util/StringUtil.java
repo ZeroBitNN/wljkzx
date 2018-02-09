fba5b78cadaa3011ff385e7e3aacd266c773c11f
zerobit <leonngx@hotmail.com>
2018-02-09 15:30:09 +0800

    修改导包错误

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -1,12 +1,8 @@
 package util;
 
-import static org.junit.Assert.*;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
-import org.junit.Test;
 
 public class StringUtil {
 	/**
